9d791a08b3ef9130ea2f11415b8d309b13fe2f48
Robin Gentele <robingentele@hotmail.com>
2026-03-12 00:17:15 +0100

    Initial Commit

---
+++ IOS_Kassensystem/IOS_KassensystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS_KassensystemApp.swift
+//  IOS_Kassensystem
+//
+//  Created by Robin Gentele on 12.03.26.
+//
+
+import SwiftUI
+
+@main
+struct IOS_KassensystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
